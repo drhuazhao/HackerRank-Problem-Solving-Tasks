4713dc13a5297f8394a7c24c988bb3a8edc0cee3
Hua Zhao <zhaohua819@gmail.com>
2022-08-31 20:55:36 +0100

    Viral Advertising in Java8

---
+++ Viral Advertising.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'viralAdvertising' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int viralAdvertising(int n) {
+    // Write your code here
+    int result =0;
+    int day1People_number =5;
+    int count_people_number = 0;
+    int share_friends =0;
+    for (int i =0; i<n;i++){
+        if(i==0){
+            count_people_number = (day1People_number/2);
+            result = result + count_people_number;
+        }
+        else{
+            share_friends= count_people_number*3;
+            count_people_number= share_friends/2;
+            result = result + count_people_number;
+            
+        }
+        
+    }
+    return(result);
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.viralAdvertising(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
