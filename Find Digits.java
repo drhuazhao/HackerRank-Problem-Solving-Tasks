96c0d05d8bde9912b009db37285c80828393301b
Hua Zhao <zhaohua819@gmail.com>
2022-09-08 11:53:48 +0100

    Find Digits in Java8

---
+++ Find Digits.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'findDigits' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int findDigits(int n) {
+    // Write your code here
+    
+    int counting_result =0;
+    
+    String list_num = String.valueOf(n);
+    for (int i =0; i<list_num.length();i++ ){
+        int j = Character.digit(list_num.charAt(i),10);
+       
+        if (j !=0){
+            if(n%j ==0){
+                counting_result = counting_result+1;
+                
+                
+            }
+            else{;}
+        }
+        else{
+            ;
+        }
+        
+        
+    }
+  
+    return(counting_result);
+    
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int result = Result.findDigits(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
