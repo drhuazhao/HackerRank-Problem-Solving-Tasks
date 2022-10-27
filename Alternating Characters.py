626e6cfd414d7b772749e36c4d6f2a45226e833b
Hua Zhao <zhaohua819@gmail.com>
2022-10-27 15:27:30 +0100

    Alternating Characters in Python3

---
+++ Alternating Characters.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'alternatingCharacters' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def alternatingCharacters(s):
+    # Write your code here
+
+    i =0
+    counting =0
+    while i<len(s):
+   
+        if i<len(s)-1:
+            if s[i] == s[i+1]:
+                counting = counting+1
+        else:
+                pass
+        i = i+1
+        
+    return(counting)
+        
+        
+        
+    
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
+
